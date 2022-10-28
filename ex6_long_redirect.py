2cdf864b3b444f477e9181f1043dbde9595e6b04
Artemeva <call.7.62@gmail.com>
2022-10-28 14:50:21 +0300

    clanup

--- ex6_long_redirect.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-
-redirect_count = len(response.history)
-final_url = response.url
-
-print("Число редиректов: ", redirect_count)
-print("Конечный URL: ",final_url)
