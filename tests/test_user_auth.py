262d97ecc4964d212d02689946f86ae97a78f216
Artemeva <call.7.62@gmail.com>
2022-10-28 15:16:42 +0300

    lesson7

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,28 +1,24 @@
 import pytest
 import requests
+from lib.base_case import BaseCase
 
-class TestUserAuth:
+class TestUserAuth(BaseCase):
     exclude_params = [
         ("no_cookie"),
         ("no_token")
     ]
 
     def setup(self):
-    data = {
-        'email': 'vinkotov@example.com',
-        'password': '1234'
-    }
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
 
-    response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
 
-    assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-    assert "x-csrf-token" in response1.headers, "There is no csrf token header in the response"
-    assert "user_id" in response1.json(), "There is no user id in the response"
-
-    self.auth_sid = response1.cookies.get("auth_sid")
-    self.token = response1.headers.get("x-csrf-token")
-    self.user_id_from_auth = response1.json()["user_id"]
-
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_from_auth = self.get_json_value(response1, "user_id")
 
     def test_user_auth(self):
 
