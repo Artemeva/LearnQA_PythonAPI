74f12f3e96f37c6fa23224e44d5882cbae23b5e1
Artemeva <call.7.62@gmail.com>
2022-11-05 14:37:17 +0300

    ex16

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -32,3 +32,26 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName", "lastName"]
 
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_other_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+
+        response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+        user_id_from_auth = self.get_json_value(response1, "user_id")
+
+        response2 = requests.get(
+            f"https://playground.learnqa.ru/api/user/1",
+            cookies={"auth_sid":auth_sid},
+            headers={"x-csrf-token":token}
+        )
+
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_no_key(response2, "email")
+        Assertions.assert_json_has_no_key(response2, "firstName")
+        Assertions.assert_json_has_no_key(response2, "lastName")
